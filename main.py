150177d6647e8dff50f28fdabd473cbd4ddd0b01
DhruvP <109266726+DhruvPathak50@users.noreply.github.com>
2024-08-23 16:54:29 +0530

    update1

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-from bs4 import BeautifulSoup
-import requests
+import os
+import smtplib
+from dotenv import load_dotenv
 
-response = requests.get("https://www.amazon.com/SAMSUNG-27-inch-Border-Less-FreeSync-LF27T350FHNXZA/dp/B08FF3JQ28/ref=sr_1_3?crid=2ALUPMEVELDQJ&dib=eyJ2IjoiMSJ9.H5GDIy5seBP0_Yw4-i4uuX9Qt4rQ_s1_KxYMGrzzvlTMvcB0HUsZeqO5bhawQtZmItcayX8vOnhstPFNcMgsJ1snmJUbROa0NPQ1Ai8g4Mc1W7142Ju3xPgW2JgHsXWCrt76oe5C7gsHnfkmDY26BNnEPFZQAHPOWbEBlq-7piTGJIh6X1iCitkrmYiHwmfsWWrkHXcdHF6_3R4kEBasskxAOiMQcaQHjfFzNbzCYZk.3XOb4BQIVwDBfbdFuBzxYWSm0nkYmSacZpOhJts1nxs&dib_tag=se&keywords=monitor%2Bsamsung&qid=1724405641&sprefix=monitor%2Bsam%2Caps%2C431&sr=8-3&th=1")
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.amazon.com/SAMSUNG-27-inch-Border-Less-FreeSync-LF27T350FHNXZA/dp/B08FF3JQ28/ref=sr_1_5?crid=B3NOBWECR116&dib=eyJ2IjoiMSJ9.HkCfZ_3N4KnLA4nkOFIBY06feBVS_BBME5iwKwRw70fEmKfR8IDjXlnumnAkTqIlF6w27iOfuPeQXWWjk3nEEIW3xEapCQykyHGWnC9nXJX4F6CdpQP9K8y16YANp7rrkJpmf50oKp30GPhBLMgRjivLNL2lYsQuLi-FuaI_JxDawpQEhvNmYB7lNpPjPTJ77bztlwb4uzBXgMChz-LG89ZVv5Msq095AutMkfwYVBI.ioyzsksnHbpCXB6n_xFDq83lOa_ohGv05bH3CcwhXKw&dib_tag=se&keywords=samsung%2Bmonitor&qid=1724410170&sprefix=sams%2Caps%2C501&sr=8-5&th=1"
+load_dotenv()
+response = requests.get(url)
 data = response.text
 # print(data)
 
 bs = BeautifulSoup(data, 'html.parser')
 # print(bs.prettify())
 tag = bs.find(name="span", class_="a-offscreen")
-price = tag.getText()
-print(price)
+# tag = bs.select_one(selector="span span .a-price-whole")
+# print(tag)
+
+current_price = float(tag.getText()[1:])
+
+# print(current_price)
+target_price = 87.00
+if current_price <= target_price:
+    # email me
+    smtp = os.getenv('SMTP_ADDRESS')
+    email = os.getenv('EMAIL_ADDRESS')
+    password = os.getenv('EMAIL_PASSWORD')
+    with smtplib.SMTP(host=smtp) as s:
+        s.starttls()
+        s.login(user=email, password=password)
+        s.sendmail(from_addr=email, to_addrs=email, msg=f"Subject: Buy this "
+                                                                                                f"monitor Now!\n\nPrice "
+                                                                                                f"of this samsung monitor "
+                                                                                                f"has fallen off to "
+                                                                                                f"{current_price} which "
+                                                                                                f"is below your set "
+                                                                                                f"target price."
+                   f"URL = {url}")
